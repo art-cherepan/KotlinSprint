f08414fd1fcb5323d074231c1eaa6a01ec518460
Artem Cherepanov <cherepanov_aa@drom.ru>
2024-08-19 06:59:37 +0700

    KS-2-5 Вывести итоговую сумму по депозиту

---
+++ src/main/kotlin/lesson2/task5.kt
@@ -0,0 +1,16 @@
+package lesson2
+
+import kotlin.math.pow
+
+fun main () {
+    val startCapital = 70_000
+    val annualInterestRate = 16.7
+    val depositTerm = 20
+    val totalAmount = calculateTotalAmount(startCapital, annualInterestRate, depositTerm)
+
+    println("Итоговая сумма: %.3f".format(totalAmount)) //почему-то результат выводит через заяптую, а не через точку (дробная часть)
+}
+
+fun calculateTotalAmount(startCapital: Int, annualInterestRate: Double, depositTerm: Int): Double {
+    return startCapital * (1 + annualInterestRate / 100).pow(depositTerm)
+}
