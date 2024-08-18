6e8f98ad2b8fd7deb7e4f91ff5720611a3de7e28
Artem Cherepanov <cherepanov_aa@drom.ru>
2024-08-18 21:36:12 +0700

    KS-2-3 Избавиться от магической константы

--- src/main/kotlin/lesson2/task3.kt
+++ src/main/kotlin/lesson2/task3.kt
@@ -4,15 +4,16 @@ fun main() {
     val trainStartingHour = 9
     val trainStartingMinute = 39
     val trainRoadDurationMinuteCount = 457
+    val minutesPerHour = 60
 
-    val trainRoadDurationHourCount = trainRoadDurationMinuteCount / 60
-    val trainRoadDurationMinuteRemainderCount = trainRoadDurationMinuteCount % 60
+    val trainRoadDurationHourCount = trainRoadDurationMinuteCount / minutesPerHour
+    val trainRoadDurationMinuteRemainderCount = trainRoadDurationMinuteCount % minutesPerHour
 
     val trainFinishHour =
             trainStartingHour +
             trainRoadDurationHourCount +
-            (trainStartingMinute + trainRoadDurationMinuteRemainderCount) / 60
-    val trainFinishMinute = (trainStartingMinute + trainRoadDurationMinuteRemainderCount) % 60
+            (trainStartingMinute + trainRoadDurationMinuteRemainderCount) / minutesPerHour
+    val trainFinishMinute = (trainStartingMinute + trainRoadDurationMinuteRemainderCount) % minutesPerHour
 
     println("Прибытие в $trainFinishHour:$trainFinishMinute")
 }
