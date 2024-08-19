6aaa8208ca57c6fac6474bced24692d03299fdef
Artem Cherepanov <cherepanov_aa@drom.ru>
2024-08-20 06:31:14 +0700

    KS-3-3 Вывести таблицу умножения для введенного числа

---
+++ src/main/kotlin/lesson3/task3.kt
@@ -0,0 +1,20 @@
+package lesson3
+
+fun main() {
+    print("Введите число: ")
+    val number = readln().toInt()
+
+    val multiplicationTable = """
+        $number * 1 = ${number * 1}
+        $number * 2 = ${number * 2}
+        $number * 3 = ${number * 3}
+        $number * 4 = ${number * 4}
+        $number * 5 = ${number * 5}
+        $number * 6 = ${number * 6}
+        $number * 7 = ${number * 7}
+        $number * 8 = ${number * 8}
+        $number * 9 = ${number * 9}
+    """.trimIndent()
+
+    println(multiplicationTable)
+}
