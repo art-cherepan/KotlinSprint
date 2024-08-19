7dd814e945895c475d19544902915c53fcab9570
Artem Cherepanov <cherepanov_aa@drom.ru>
2024-08-19 21:48:30 +0700

    KS-2-4 Избавиться от магических чисел

--- src/main/kotlin/lesson2/task4.kt
+++ src/main/kotlin/lesson2/task4.kt
@@ -3,14 +3,15 @@ package lesson2
 fun main() {
     val crystalOreWithoutBuffCount = 7
     val ironOreWithoutBuffCount = 11
+    val buffPercent = 20
 
-    val crystalOreBuff = calculateBuff(crystalOreWithoutBuffCount)
-    val ironOreBuff = calculateBuff(ironOreWithoutBuffCount)
+    val crystalOreBuff = calculateBuff(crystalOreWithoutBuffCount, buffPercent)
+    val ironOreBuff = calculateBuff(ironOreWithoutBuffCount, buffPercent)
 
     println("Бафф от кристальной руды: $crystalOreBuff")
     println("Бафф от железной руды: $ironOreBuff")
 }
 
-fun calculateBuff(oreCount: Int): Int {
-    return (oreCount * 0.2).toInt()
+fun calculateBuff(oreCount: Int, buffPercent: Int): Int {
+    return (oreCount * buffPercent) / 100
 }
