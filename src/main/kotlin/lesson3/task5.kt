3caa8ab7720eb0b1878ffe14fa1d0e92918253a6
Artem Cherepanov <cherepanov_aa@drom.ru>
2024-08-22 22:14:17 +0700

    KS-3-5 Изменить вызов сплит для нескольких разделителей

--- src/main/kotlin/lesson3/task5.kt
+++ src/main/kotlin/lesson3/task5.kt
@@ -2,15 +2,9 @@ package lesson3
 
 fun main() {
     val logStr = "D2-D4;0"
+    val logStrToList = logStr.split("-",";")
 
-    val logStrToList = logStr.split(";")
-    val startFinishField = logStrToList.first()
-    val playerMove = logStrToList.last()
-
-    val startField = startFinishField.split("-").first()
-    val finishField = startFinishField.split("-").last()
-
-    println("Начальное поле: $startField")
-    println("Конечное поле: $finishField")
-    println("Ход игрока: $playerMove")
+    println("Начальное поле: ${logStrToList[0]}")
+    println("Конечное поле: ${logStrToList[1]}")
+    println("Ход игрока: ${logStrToList[2]}")
 }
