929c17a05f7e648f74b61f9c2f9b99995bdf685f
Artem Cherepanov <cherepanov_aa@drom.ru>
2024-08-23 15:59:29 +0700

    KS-4-1 Вынести в константу

--- src/main/kotlin/lesson4/task1.kt
+++ src/main/kotlin/lesson4/task1.kt
@@ -1,11 +1,11 @@
 package lesson4
 
 fun main() {
-    val numberOfTables = 13
-
     val todayNumberOfReservationTables = 13
     val tomorrowNumberOfReservationTables = 9
 
     println("Доступность столиков на сегодня: ${todayNumberOfReservationTables != numberOfTables}")
     println("Доступность столиков на завтра: ${tomorrowNumberOfReservationTables != numberOfTables}")
 }
+
+const val NUMBER_OF_TABLES = 13
