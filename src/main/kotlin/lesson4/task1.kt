bc891fbcbd491137af97c63a4170da8cc742059b
Artem Cherepanov <cherepanov_aa@drom.ru>
2024-08-23 15:59:42 +0700

    KS-4-1 Изменить условие

--- src/main/kotlin/lesson4/task1.kt
+++ src/main/kotlin/lesson4/task1.kt
@@ -4,8 +4,8 @@ fun main() {
     val todayNumberOfReservationTables = 13
     val tomorrowNumberOfReservationTables = 9
 
-    println("Доступность столиков на сегодня: ${todayNumberOfReservationTables != numberOfTables}")
-    println("Доступность столиков на завтра: ${tomorrowNumberOfReservationTables != numberOfTables}")
+    println("Доступность столиков на сегодня: ${todayNumberOfReservationTables < NUMBER_OF_TABLES}")
+    println("Доступность столиков на завтра: ${tomorrowNumberOfReservationTables < NUMBER_OF_TABLES}")
 }
 
 const val NUMBER_OF_TABLES = 13
