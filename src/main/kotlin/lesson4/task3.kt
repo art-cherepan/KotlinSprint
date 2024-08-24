4aa0fcf5ddde2177266f46a4cf6ef75e3b1d5f5a
Artem Cherepanov <cherepanov_aa@drom.ru>
2024-08-24 16:56:05 +0700

    KS-4-3 Определить благоприятные условия

---
+++ src/main/kotlin/lesson4/task3.kt
@@ -0,0 +1,28 @@
+package lesson4
+
+fun main() {
+    val isSunnyWeather = true
+    val isOpenAwning = true
+    val airHumidity = 20
+    val timeOfYear = "зима"
+
+    val checkFavorableConditions = isFavorableConditions(
+        isSunnyWeather,
+        isOpenAwning,
+        airHumidity,
+        timeOfYear,
+    )
+
+    println("Благоприятные ли условия сейчас для роста бобовых? $checkFavorableConditions")
+}
+
+fun isFavorableConditions(
+    isSunnyWeather: Boolean,
+    isOpenAwning: Boolean,
+    airHumidity: Int,
+    timeOfYear: String,
+): Boolean {
+    val favorableTimesOfYear = setOf("весна", "лето", "осень")
+
+    return isSunnyWeather && isOpenAwning && airHumidity == 20 && favorableTimesOfYear.contains(timeOfYear)
+}
