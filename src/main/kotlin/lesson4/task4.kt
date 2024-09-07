bf77ecebff3656dc8d71843fd531ae9a3ecbc2f1
Artem Cherepanov <cherepanov_aa@drom.ru>
2024-09-07 21:26:24 +0700

    KS-4-4 Реализовать логику в основной программе

--- src/main/kotlin/lesson4/task4.kt
+++ src/main/kotlin/lesson4/task4.kt
@@ -1,44 +1,15 @@
 package lesson4
 
 fun main() {
-    try {
-        println("Введите номер дня тренировки:")
-        val numberOfTrainingDay = readln().toInt()
-        val programOfTraining = getProgramOfTraining(numberOfTrainingDay)
+    println("Введите номер дня тренировки:")
 
-        println("""
-        Упражнения для рук:    ${programOfTraining["exercisesForHands"]}
-        Упражнения для ног:    ${programOfTraining["exercisesForLegs"]}
-        Упражнения для спины:  ${programOfTraining["exercisesForRear"]}
-        Упражнения для пресса: ${programOfTraining["exercisesForAbdominal"]}
-    """.trimIndent())
-    } catch (e: IllegalArgumentException) {
-        println(e.message)
-    }
+    val numberOfTrainingDay = readln().toInt()
+    val isHonestTrainingDay = (numberOfTrainingDay % 2 == 0)
+
+    println("""
+    Упражнения для рук:    $isHonestTrainingDay
+    Упражнения для ног:    ${!isHonestTrainingDay}
+    Упражнения для спины:  ${!isHonestTrainingDay}
+    Упражнения для пресса: $isHonestTrainingDay
+""".trimIndent())
 }
-
-fun getProgramOfTraining(numberOfTrainingDay: Int): Map<String, Boolean> {
-    if (numberOfTrainingDay < MINIMAL_NUMBER_OF_TRAINING_DAY) {
-        throw IllegalArgumentException("Number of training day must not be less than one.")
-    }
-
-    val honestTrainingDay = mapOf(
-        "exercisesForHands" to true,
-        "exercisesForLegs" to false,
-        "exercisesForRear" to false,
-        "exercisesForAbdominal" to true,
-    )
-
-    val oddTrainingDay = mapOf(
-        "exercisesForHands" to false,
-        "exercisesForLegs" to true,
-        "exercisesForRear" to true,
-        "exercisesForAbdominal" to false,
-    )
-
-    if (numberOfTrainingDay % 2 == 0) return honestTrainingDay
-
-    return oddTrainingDay
-}
-
-const val MINIMAL_NUMBER_OF_TRAINING_DAY = 1
