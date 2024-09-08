53ca403933c791de66c2eaa230f0d44083fb3e53
Artem Cherepanov <cherepanov_aa@drom.ru>
2024-09-08 18:50:51 +0700

    KS-4-5 Избавиться от магических чисел

--- src/main/kotlin/lesson4/task5.kt
+++ src/main/kotlin/lesson4/task5.kt
@@ -15,11 +15,15 @@ fun main() {
 
     val canSail =
         (!isShipsHullDamageExist &&
-        crewComposition in 55 .. 70 &&
-        numberOfSupplyCrates > 50) ||
-        (crewComposition == 70 &&
+        crewComposition in MINIMAL_CREW_COMPOSITION_PERSON_NUMBER .. MAXIMAL_CREW_COMPOSITION_PERSON_NUMBER &&
+        numberOfSupplyCrates > MINIMAL_SUPPLY_CRATES_NUMBER) ||
+        (crewComposition == MAXIMAL_CREW_COMPOSITION_PERSON_NUMBER &&
         isFavorableWeather &&
-        numberOfSupplyCrates >= 50)
+        numberOfSupplyCrates >= MINIMAL_SUPPLY_CRATES_NUMBER)
 
     println("Корабль может отправиться в плавание: $canSail")
 }
+
+const val MINIMAL_CREW_COMPOSITION_PERSON_NUMBER = 55
+const val MAXIMAL_CREW_COMPOSITION_PERSON_NUMBER = 70
+const val MINIMAL_SUPPLY_CRATES_NUMBER = 50
