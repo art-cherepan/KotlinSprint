7c002948504c07640294e20e926d8b9273112641
Artem Cherepanov <cherepanov_aa@drom.ru>
2024-09-08 18:31:13 +0700

    KS-5-3 Переделать логику в основной программе

--- src/main/kotlin/lesson5/task3.kt
+++ src/main/kotlin/lesson5/task3.kt
@@ -2,29 +2,27 @@ package lesson5
 
 fun main() {
     println("Введите 1 число:")
-    val firstUserNumber = readln().toInt()
+    val firstNumber = readln().toInt()
 
     println("Введите 2 число:")
-    val secondUserNumber = readln().toInt()
+    val secondNumber = readln().toInt()
 
-    checkLucky(firstUserNumber, secondUserNumber)
-}
-
-fun checkLucky(firstNumber: Int, secondNumber: Int) {
-    if (firstNumber == FIRST_LUCKY_NUMBER || firstNumber == SECOND_LUCKY_NUMBER) {
-        if ((firstNumber != secondNumber) && (secondNumber == FIRST_LUCKY_NUMBER) || (secondNumber == SECOND_LUCKY_NUMBER)) {
+    if ((firstNumber == FIRST_LUCKY_NUMBER && secondNumber == SECOND_LUCKY_NUMBER) ||
+        (firstNumber == SECOND_LUCKY_NUMBER && secondNumber == FIRST_LUCKY_NUMBER)) {
             println("Поздравляем! Вы выиграли главный приз!")
-        } else {
+    } else if (
+            firstNumber == FIRST_LUCKY_NUMBER ||
+            firstNumber == SECOND_LUCKY_NUMBER ||
+            secondNumber == FIRST_LUCKY_NUMBER ||
+            secondNumber == SECOND_LUCKY_NUMBER
+        ) {
             println("Вы выиграли утешительный приз!")
+        } else {
+            println("Неудача!")
         }
-    } else if (secondNumber == FIRST_LUCKY_NUMBER || secondNumber == SECOND_LUCKY_NUMBER) {
-        println("Вы выиграли утешительный приз!")
-    } else {
-        println("Неудача!")
-    }
 
     println("Для победы нужно было отгадать числа $FIRST_LUCKY_NUMBER и $SECOND_LUCKY_NUMBER")
 }
 
-const val FIRST_LUCKY_NUMBER = 8
+const val FIRST_LUCKY_NUMBER = 21
 const val SECOND_LUCKY_NUMBER = 21
