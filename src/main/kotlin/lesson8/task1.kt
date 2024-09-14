389f01b6627d18f31b71360f088e4522ce7146df
Artem Cherepanov <cherepanov_aa@drom.ru>
2024-09-14 21:50:10 +0700

    KS-8-1 Упростить код

--- src/main/kotlin/lesson8/task1.kt
+++ src/main/kotlin/lesson8/task1.kt
@@ -2,11 +2,6 @@ package lesson8
 
 fun main() {
     val days = intArrayOf(20, 32, 12, 32, 54, 13, 21)
-    var sum = 0
 
-    for (viewings in days) {
-        sum += viewings
-    }
-
-    println("Количество просмотров рекламы за неделю: $sum")
+    println("Количество просмотров рекламы за неделю: ${days.sum()}")
 }
