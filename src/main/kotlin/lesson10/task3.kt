de5d7f83713ee8bb33a29974b595b4f852b5faca
Artem Cherepanov <cherepanov_aa@drom.ru>
2024-09-23 07:47:08 +0700

    KS-10-3 Добавить функцию генерации пароля

---
+++ src/main/kotlin/lesson10/task3.kt
@@ -0,0 +1,30 @@
+package lesson10
+
+fun main() {
+    println("Введите длину пароля:")
+    val passwordLength = readln().toInt()
+
+    println("Ваш пароль: ${generatePassword(passwordLength)}")
+}
+
+fun generatePassword(passwordLength: Int): String {
+    val specSymbols: Array<Char> = arrayOf(
+        '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ' '
+    )
+
+    val digits = 0 .. 9
+    val startIndex = 0
+    var strPasswordResult = ""
+
+    for (counter in startIndex .. passwordLength) {
+        if (counter % EVEN_CHECK == 0) {
+            strPasswordResult += digits.random()
+        } else {
+            strPasswordResult += specSymbols.random()
+        }
+    }
+
+    return strPasswordResult
+}
+
+const val EVEN_CHECK = 2
