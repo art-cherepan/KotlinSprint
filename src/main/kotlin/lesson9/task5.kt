800dd9e83ee6cf0e4461edb7ad2670d201f28f19
Artem Cherepanov <cherepanov_aa@drom.ru>
2024-09-24 15:18:18 +0700

    KS-9-5 Изменить реализацию получения ингредиентов через множество

--- src/main/kotlin/lesson9/task5.kt
+++ src/main/kotlin/lesson9/task5.kt
@@ -3,19 +3,15 @@ package lesson9
 import java.util.*
 
 fun main() {
-    val userIngredients = mutableSetOf<String>()
+    println("Введите ингредиенты по очереди:")
+    val userIngredients = MutableList<String?>(INGREDIENTS_COUNT) { readln() }
 
-    while (userIngredients.size < COUNT_OF_UNIQUE_INGREDIENTS) {
-        println("Введите ингредиент:")
-        val userIngredient = readln()
+    userIngredients.sortBy { it }
 
-        userIngredients.add(userIngredient)
-    }
-
-    val sortedUserIngredients = userIngredients.sorted().joinToString(separator = ", ")
-    val sortedUserIngredientsCapitalize = sortedUserIngredients.replaceFirstChar { it.titlecase(Locale.getDefault()) }
+    val userIngredientsToString = userIngredients.toSet().joinToString(separator = ", ")
+    val sortedUserIngredientsCapitalize = userIngredientsToString.replaceFirstChar { it.titlecase(Locale.getDefault()) }
 
     println(sortedUserIngredientsCapitalize)
 }
 
-const val COUNT_OF_UNIQUE_INGREDIENTS = 5
+const val INGREDIENTS_COUNT = 5
