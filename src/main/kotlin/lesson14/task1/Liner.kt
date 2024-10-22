f671eb7e7062c10fade187ee7672596163562a99
Artem Cherepanov <cherepanov_aa@drom.ru>
2024-10-22 22:19:06 +0700

    KS-14-1 Перенести классы в Main.kt

--- src/main/kotlin/lesson14/task1/Liner.kt
+++
@@ -1,9 +0,0 @@
-package lesson14.task1
-
-open class Liner (
-    val speed: Int = 100,
-    val liftingCapacity: Int = 20,
-    val passengerCapacity: Int = 1000,
-    val toCrashIce: Boolean = false,
-) {
-}
--- src/main/kotlin/lesson14/task1/Main.kt
+++ src/main/kotlin/lesson14/task1/Main.kt
@@ -4,8 +4,36 @@ fun main() {
     val liner = Liner()
     val cargo = Cargo()
     val icebreaker = Icebreaker()
+}
 
-    println(liner.speed)
-    println(cargo.speed)
-    println(icebreaker.speed)
+open class Liner (
+    val speed: Int = 100,
+    val liftingCapacity: Int = 20,
+    val passengerCapacity: Int = 1000,
+    val toCrashIce: Boolean = false,
+) {
+}
+
+class Cargo(
+    speed: Int = 80,
+    liftingCapacity: Int = 50,
+    passengerCapacity: Int = 700,
+): Liner(
+    speed,
+    liftingCapacity,
+    passengerCapacity,
+) {
+}
+
+class Icebreaker(
+    speed: Int = 70,
+    liftingCapacity: Int = 30,
+    passengerCapacity: Int = 300,
+    toCrashIce: Boolean = true,
+) : Liner(
+    speed,
+    liftingCapacity,
+    passengerCapacity,
+    toCrashIce,
+) {
 }
