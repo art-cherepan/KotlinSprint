a63e2f9bc0e5b62b83b1bb1ff7f61b8a4f223635
Artem Cherepanov <cherepanov_aa@drom.ru>
2024-11-08 07:40:43 +0700

    KS-15-5 Добавить интерфейсы, переделать логику программы

--- src/main/kotlin/lesson15/task5/Main.kt
+++ src/main/kotlin/lesson15/task5/Main.kt
@@ -1,9 +1,9 @@
 package lesson15.task5
 
 fun main() {
-    val mazda = Car("Mazda 6")
-    val bmw = Car("BMW X3")
-    val zil = Cargo("ЗИЛ")
+    val mazda = Car("Mazda 6", 3)
+    val bmw = Car("BMW X3", 3)
+    val zil = Cargo("ЗИЛ", 1, 3000.0)
 
     mazda.pickUpPassenger(3)
     bmw.pickUpPassenger(3)
@@ -27,23 +27,19 @@ interface Movable {
     fun stopMovement()
 }
 
-interface PassengersTransportation {
-    val maxPassengerCount: Int
-    var onBoardPassengerCount: Int
-
+interface Transportable {
     fun pickUpPassenger(passengerCount: Int): Boolean
     fun dropOffPassenger(passengerCount: Int): Boolean
 }
 
-interface CargoTransportation {
-    val maxCargoMass: Double
-    var currentCargoMass: Double
-
+interface Shippable {
     fun pickUpCargo(cargoMass: Double): Boolean
     fun dropOffCargo(cargoMass: Double): Boolean
 }
 
-abstract class Transport(val name: String) : Movable, PassengersTransportation {
+abstract class Transport(val name: String, private val maxPassengerCount: Int) : Movable, Transportable {
+    private var onBoardPassengerCount: Int = 0
+
     override fun startMovement() {
         println("$name начал движение")
     }
@@ -55,6 +51,7 @@ abstract class Transport(val name: String) : Movable, PassengersTransportation {
     override fun pickUpPassenger(passengerCount: Int): Boolean {
         if ((onBoardPassengerCount + passengerCount) <= maxPassengerCount) {
             onBoardPassengerCount += passengerCount
+            println("$name забрал $passengerCount пассажира(ов)")
 
             return true
         }
@@ -65,6 +62,7 @@ abstract class Transport(val name: String) : Movable, PassengersTransportation {
     override fun dropOffPassenger(passengerCount: Int): Boolean {
         if ((onBoardPassengerCount - passengerCount) >= 0) {
             onBoardPassengerCount -= passengerCount
+            println("$name высадил $passengerCount пассажира(ов)")
 
             return true
         }
@@ -73,20 +71,22 @@ abstract class Transport(val name: String) : Movable, PassengersTransportation {
     }
 }
 
-class Car(name: String) : Transport(name = name) {
-    override val maxPassengerCount = 3
-    override var onBoardPassengerCount = 0
-}
+class Car(name: String, maxPassengerCount: Int) : Transport(name, maxPassengerCount)
 
-class Cargo(name: String) : Transport(name = name), CargoTransportation {
-    override val maxPassengerCount = 1
-    override var onBoardPassengerCount = 0
-    override val maxCargoMass = 3000.0
-    override var currentCargoMass = 0.0
+class Cargo(
+    name: String,
+    maxPassengerCount: Int,
+    private val maxCargoMass: Double
+) : Transport(
+    name,
+    maxPassengerCount
+), Shippable {
+    private var currentCargoMass: Double = 0.0
 
     override fun pickUpCargo(cargoMass: Double): Boolean {
         if ((currentCargoMass + cargoMass) <= maxCargoMass ) {
             currentCargoMass += cargoMass
+            println("$name загрузил $cargoMass тонн груза")
 
             return true
         }
@@ -97,6 +97,7 @@ class Cargo(name: String) : Transport(name = name), CargoTransportation {
     override fun dropOffCargo(cargoMass: Double): Boolean {
         if ((currentCargoMass - cargoMass) >= 0.0) {
             currentCargoMass -= cargoMass
+            println("$name разгрузил $cargoMass тонн груза")
 
             return true
         }
