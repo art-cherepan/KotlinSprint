3a43fe4dfc55c58adf3507214d42074fcdc308bd
Artem Cherepanov <cherepanov_aa@drom.ru>
2024-11-23 12:42:44 +0700

    KS-17-5 Добавить класс пользователя

---
+++ src/main/kotlin/lesson17/task5/Main.kt
@@ -0,0 +1,31 @@
+package lesson17.task5
+
+fun main() {
+    val user = User(
+        login = "login",
+        password = "123222",
+    )
+    println("Логин: ${user.login}\nПароль: ${user.password}")
+
+    user.login = "newLogin"
+    user.password = "123"
+    println("Новый логин: ${user.login}\nНовый пароль: ${user.password}")
+}
+
+class User(login: String, password: String) {
+    var login = login
+        set(value) {
+            field = value
+            println("Логин успешно изменен")
+        }
+
+    var password = password
+        get() {
+            val regex = ".".toRegex()
+            return field.replace(regex, "*")
+        }
+
+        set(value) {
+            println("Вы не можете изменить пароль")
+        }
+}
