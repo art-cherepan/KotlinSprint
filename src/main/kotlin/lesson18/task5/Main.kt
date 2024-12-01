4c9edde8b2799a877f735ddef5f40ac7192b1994
Artem Cherepanov <cherepanov_aa@drom.ru>
2024-12-02 06:33:13 +0700

    KS-18-5 Переименовать методы

--- src/main/kotlin/lesson18/task5/Main.kt
+++ src/main/kotlin/lesson18/task5/Main.kt
@@ -3,36 +3,36 @@ package lesson18.task5
 fun main() {
     val screen = Screen()
 
-    screen.draw(1,2)
-    screen.draw(1.3.toFloat(), 2.4.toFloat())
-    screen.draw(10, 20)
-    screen.draw(10.3.toFloat(), 21.1.toFloat())
-    screen.draw(11, 21, 4)
-    screen.draw(11.3.toFloat(), 22.4.toFloat(), 7)
+    screen.drawDot(1,2)
+    screen.drawDot(1.3.toFloat(), 2.4.toFloat())
+    screen.drawSquare(10, 20, 7)
+    screen.drawSquare(10.3.toFloat(), 21.1.toFloat(), 8)
+    screen.drawCircle(11, 21, 4)
+    screen.drawCircle(11.3.toFloat(), 22.4.toFloat(), 7)
 }
 
 class Screen {
-    fun draw(x: Int, y: Int, sideLength: Float) {
+    fun drawSquare(x: Int, y: Int, sideLength: Int) {
         println("Нарисован квадрат с координатами $x и $y и длиной стороны $sideLength")
     }
 
-    fun draw(x: Float, y: Float, sideLength: Float) {
+    fun drawSquare(x: Float, y: Float, sideLength: Int) {
         println("Нарисован квадрат с координатами ${x.toInt()} и ${y.toInt()} и длиной стороны $sideLength")
     }
 
-    fun draw(x: Int, y: Int, radius: Int) {
+    fun drawCircle(x: Int, y: Int, radius: Int) {
         println("Нарисован круг с координатами $x и $y и радиусом $radius")
     }
 
-    fun draw(x: Float, y: Float, radius: Int) {
+    fun drawCircle(x: Float, y: Float, radius: Int) {
         println("Нарисован круг с координатами ${x.toFloat()} и ${y.toFloat()} и радиусом $radius")
     }
 
-    fun draw(x: Int, y: Int) {
+    fun drawDot(x: Int, y: Int) {
         println("Нарисована точка с координатами $x и $y")
     }
 
-    fun draw(x: Float, y: Float) {
+    fun drawDot(x: Float, y: Float) {
         println("Нарисована точка с координатами ${x.toInt()} и ${y.toInt()}")
     }
 }
